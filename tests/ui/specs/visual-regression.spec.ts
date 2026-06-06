b1f70f6dee38abcfef35e2b50f86923c6ad6ea21
ccdita <christaldita@gmail.com>
2026-06-06 17:53:57 -0400

    Add Ch5 notes

--- tests/ui/specs/visual-regression.spec.ts
+++ tests/ui/specs/visual-regression.spec.ts
@@ -15,6 +15,7 @@ import {
     Target
   } from '@applitools/eyes-playwright';
 
+// Define which runner Applitools will use
 export const USE_ULTRAFAST_GRID: boolean = true;
 export let Batch: BatchInfo;
 export let Config: Configuration;
@@ -23,26 +24,48 @@ let eyes: Eyes;
 let profilePage: ProfilePage;
 let menuPage: MenuPage;
 
+// beforeAll for Applitools configuration and runner initialization
 test.beforeAll(async() => {
-    Runner = new VisualGridRunner({ testConcurrency: 5 });
-    const runnerName = 'Ultrafast Grid';
+    /**
+     * Determine which runner to use
+     * Ultrafast Grid
+     * - All test execution will be managed by Applitools in the cloud
+     * - Useful for running many browsers and devices in parallel
+     * 
+     * ClassicRunner
+     * - Manage everything locally
+     * - You must run every single browser or device in the local machine
+     * - Time-consuming because you must download and install every single browser,
+     * and manage parallelism locally
+     */
+    Runner = new VisualGridRunner({ testConcurrency: 5 }); // The free version of Applitools lets you run only one test
+    const runnerName = 'Ultrafast Grid'; // Displays on Applitools Eyes dashboard
+
+    /**
+     * A batch is a collection of checkpoints for each test suite
+     * - If we are using VisualGridRunner, we will have one batch for all the configurations since
+     * Applitools manages everything for us
+     * - If we are using ClassicRunner, each browser or device will be their own batch, since we
+     * will start and trigger every single test locally
+     */
     Batch = new BatchInfo({name: `Book Store - New Tab - ${runnerName}`});
     
     Config = new Configuration();
     
     Config.setBatch(Batch);
+    // Add different viewports, screen orientations, browsers, devices
     Config.addBrowser(800, 600, BrowserType.CHROME);
     Config.addDeviceEmulation(DeviceName.iPhone_11, ScreenOrientation.PORTRAIT);
 });
 
 
 test.beforeEach(async ({page, context}) => {
-    eyes = new Eyes(Runner, Config);
+    eyes = new Eyes(Runner, Config); // Create a new Eyes instance for each test
     await eyes.open(
-      page,
-      'Book Store App',
-      test.info().title,
-      { width: 1024, height: 768 }
+      page,  // Driver
+      'Book Store App', // App name
+      test.info().title, // Test name
+      { width: 1024, height: 768 } // View port
     );
 
     profilePage = await hooks.beforeEach(page, ProfilePage, pages.profile);
@@ -50,10 +73,11 @@ test.beforeEach(async ({page, context}) => {
 });
 
 test.afterEach(async () => {
-    await eyes.close();
+    await eyes.close(); // We must close the Eyes instance after each test to ensure the session is finished
 });
 
 test.afterAll(async() => {
+  // Forces Playwright to wait synchronously for all visual checkpoints to complete
   const results = await Runner.getAllTestResults();
   console.log('Visual test results', results);
 });
